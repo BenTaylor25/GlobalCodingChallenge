08b8451f524f836143f602db9e3774d6aa88b6a7
Ben Taylor <ben.taylor2573@gmail.com>
2022-10-07 18:06:56 +0100

    added find_divisors

---
+++ 2022/challenge_1/find_divisors.py
@@ -0,0 +1,27 @@
+def _divisors(x, a, b):
+    if a > b:
+        return set()
+
+    dvsrs = set()
+    c = a*b
+    if c < x:
+        dvsrs |= _divisors(x, a+1, b)
+    elif c > x:
+        dvsrs |= _divisors(x, a, b-1)
+    else:
+        dvsrs |= {a, b} | _divisors(x, a+1, b-1)
+
+    return dvsrs
+
+def divisors(x):
+    dvsrs = {x}
+    dvsrs |= _divisors(x, 2, int(x/2))
+    
+    print(", ".join([str(x) for x in dvsrs]))
+
+def main():
+    x = 25
+    divisors(x)
+
+if __name__ == '__main__':
+    main()
